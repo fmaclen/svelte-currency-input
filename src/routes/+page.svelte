9cee3fcd8f64c4126123a2ea0d5a60707a2caaa0
Fernando Maclen <fmaclen@gmail.com>
2022-09-15 01:02:01 -0400

    General cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,14 +8,14 @@
 
 <div class="container">
 	<CurrencyInput name="cashflow" value={-42069.69} />
-	<CurrencyInput name="cashflow" value={0} />
-	<CurrencyInput name="cashflow" value={5678.9} />
-	<CurrencyInput name="cashflow" value={1234.56} isNegativeAllowed={false} />
+	<CurrencyInput name="rent" value={0} />
+	<CurrencyInput name="total" value={5678.9} />
+	<CurrencyInput name="balance" value={1234.56} isNegativeAllowed={false} />
 
-	<CurrencyInput name="cashflow" value={5678.9} {locale} {currency} />
-	<CurrencyInput name="cashflow" value={1234.56} isNegativeAllowed={false} {locale} {currency} />
-	<CurrencyInput name="cashflow" value={-42069.69} {locale} {currency} />
-	<CurrencyInput name="cashflow" value={0} {locale} {currency} />
+	<CurrencyInput name="amount" value={5678.9} {locale} {currency} />
+	<CurrencyInput name="deficit" value={1234.56} isNegativeAllowed={false} {locale} {currency} />
+	<CurrencyInput name="cost" value={-42069.69} {locale} {currency} />
+	<CurrencyInput name="exchange" value={0} {locale} {currency} />
 </div>
 
 <style lang="scss">
