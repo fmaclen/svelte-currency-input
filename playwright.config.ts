84f24e0af39e895cd7e376c23b514040b486f18a
Fernando Maclen <fmaclen@gmail.com>
2022-09-16 12:06:57 -0400

    #5 - Add support for multiple browsers in CI test runs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,26 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
+
+const multipleBrowsers = [
+	{
+		name: 'chromium',
+		use: { ...devices['Desktop Chrome'] }
+	},
+	{
+		name: 'firefox',
+		use: { ...devices['Desktop Firefox'] }
+	},
+	{
+		name: 'webkit',
+		use: { ...devices['Desktop Safari'] }
+	}
+];
 
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run dev',
 		port: 5173
-	}
+	},
+	projects: process.env.NODE_ENV === 'CI' ? multipleBrowsers : undefined
 };
 
 export default config;
