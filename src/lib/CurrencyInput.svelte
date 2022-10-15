50744f9f1f79b59d4ca14180ca19d5405faca8c8
gokullan <84223229+gokullan@users.noreply.github.com>
2022-10-15 23:04:20 +0530

    fix: formatting triggered for zeros added after fractionDigits limit (#37)
    * fix: formatting triggered for zeros added after fractionDigits limit
    
    * debugging lines removed
    
    * Update src/lib/CurrencyInput.svelte
    
    Co-authored-by: Fernando Maclen <fmaclen@gmail.com>
    Signed-off-by: gokullan <84223229+gokullan@users.noreply.github.com>
    
    * refactor: code formatting updates
    
    Signed-off-by: gokullan <84223229+gokullan@users.noreply.github.com>
    Co-authored-by: Fernando Maclen <fmaclen@gmail.com>

--- src/lib/CurrencyInput.svelte
+++ src/lib/CurrencyInput.svelte
@@ -47,33 +47,35 @@
 		.replace(/\u00A0/, ''); // e.g '0 €' > '€'
 
 	// Updates `value` by stripping away the currency formatting
-	const setUnformattedValue = (event: KeyboardEvent) => {
-		// Don't format if the user is typing a `currencyDecimal` point
-		if (event.key === currencyDecimal) return;
+	const setUnformattedValue = (event?: KeyboardEvent) => {
+		if (event) {
+			// Don't format if the user is typing a `currencyDecimal` point
+			if (event.key === currencyDecimal) return;
 
-		// Pressing `.` when the decimal point is `,` gets replaced with `,`
-		if (isDecimalComma && event.key === '.')
-			formattedValue = formattedValue.replace(/\.([^.]*)$/, currencyDecimal + '$1'); // Only replace the last occurence
+			// Pressing `.` when the decimal point is `,` gets replaced with `,`
+			if (isDecimalComma && event.key === '.')
+				formattedValue = formattedValue.replace(/\.([^.]*)$/, currencyDecimal + '$1'); // Only replace the last occurence
 
-		// Pressing `,` when the decimal point is `.` gets replaced with `.`
-		if (!isDecimalComma && event.key === ',')
-			formattedValue = formattedValue.replace(/\,([^,]*)$/, currencyDecimal + '$1'); // Only replace the last occurence
+			// Pressing `,` when the decimal point is `.` gets replaced with `.`
+			if (!isDecimalComma && event.key === ',')
+				formattedValue = formattedValue.replace(/\,([^,]*)$/, currencyDecimal + '$1'); // Only replace the last occurence
 
-		// Don't format if `formattedValue` is ['$', '-$', "-"]
-		const ignoreSymbols = [currencySymbol, `-${currencySymbol}`, '-'];
-		const strippedUnformattedValue = formattedValue.replace(' ', '');
-		if (ignoreSymbols.includes(strippedUnformattedValue)) return;
+			// Don't format if `formattedValue` is ['$', '-$', "-"]
+			const ignoreSymbols = [currencySymbol, `-${currencySymbol}`, '-'];
+			const strippedUnformattedValue = formattedValue.replace(' ', '');
+			if (ignoreSymbols.includes(strippedUnformattedValue)) return;
 
-		// Set the starting caret positions
-		inputTarget = event.target as HTMLInputElement;
+			// Set the starting caret positions
+			inputTarget = event.target as HTMLInputElement;
+
+			// Reverse the value when minus is pressed
+			if (isNegativeAllowed && event.key === '-') value = value * -1;
+		}
 
 		// Remove all characters that arent: numbers, commas, periods (or minus signs if `isNegativeAllowed`)
 		let unformattedValue = isNegativeAllowed
 			? formattedValue.replace(/[^0-9,.-]/g, '')
 			: formattedValue.replace(/[^0-9,.]/g, '');
-
-		// Reverse the value when minus is pressed
-		if (isNegativeAllowed && event.key === '-') value = value * -1;
 
 		// Finally set the value
 		if (Number.isNaN(parseFloat(unformattedValue))) {
@@ -82,7 +84,20 @@
 			// The order of the following operations is *critical*
 			unformattedValue = unformattedValue.replace(isDecimalComma ? /\./g : /\,/g, ''); // Remove all group symbols
 			if (isDecimalComma) unformattedValue = unformattedValue.replace(',', '.'); // If the decimal point is a comma, replace it with a period
+
+			// If the zero-key has been pressed
+			// and if the current `value` is the same as the `value` before the key-press
+			// formatting may need to be done (Issue #30)
+			const previousValue = value;
 			value = parseFloat(unformattedValue);
+
+			if (event && previousValue === value) {
+				// Do the formatting if the number of digits after the decimal point exceeds `fractionDigits`
+				if (unformattedValue.includes('.') && unformattedValue.split('.')[1].length > fractionDigits)
+				{
+					setFormattedValue();
+				}
+			}
 		}
 	};
 
@@ -93,6 +108,9 @@
 
 		// Apply formatting to input
 		formattedValue = isZero ? '' : formatCurrency(value, fractionDigits, 0);
+
+		// Update `value` after formatting
+		setUnformattedValue();
 
 		// New caret position
 		const endCaretPosition =
@@ -131,6 +149,7 @@
 		bind:value={formattedValue}
 		on:keydown={handleKeyDown}
 		on:keyup={setUnformattedValue}
+		on:blur={setFormattedValue}
 	/>
 </div>
 
