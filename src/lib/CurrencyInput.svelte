337895092d55e37ec88614cea037ad2fcefaf278
Fernando Maclen <fmaclen@gmail.com>
2023-10-23 09:49:35 -0700

    feat: add ability to add a custom placeholder
    Fixes #63

--- src/lib/CurrencyInput.svelte
+++ src/lib/CurrencyInput.svelte
@@ -31,7 +31,7 @@
 	export let name: string = DEFAULT_NAME;
 	export let required: boolean = false;
 	export let disabled: boolean = false;
-	export let placeholder: number | null = DEFAULT_VALUE;
+	export let placeholder: string | number | null = DEFAULT_VALUE;
 	export let autocomplete: string | null | undefined = undefined;
 	export let isNegativeAllowed: boolean = true;
 	export let isZeroNullish: boolean = false;
@@ -160,9 +160,14 @@
 		onValueChange(value);
 	};
 
+	const formatPlaceholder = (placeholder: string | number | null) => {
+		if (typeof placeholder === "number") return formatCurrency(placeholder, fractionDigits, fractionDigits);
+		if (placeholder === null) return "";
+		return placeholder;
+	};
+
 	let formattedValue = '';
-	let formattedPlaceholder =
-		placeholder !== null ? formatCurrency(placeholder, fractionDigits, fractionDigits) : '';
+	let formattedPlaceholder = formatPlaceholder(placeholder);
 	$: isNegative = value < 0;
 	$: isPositive = value > 0;
 	$: isZero = !isNegative && !isPositive;
