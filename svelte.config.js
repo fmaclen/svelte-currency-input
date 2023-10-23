c6e90c5d6bfa420440449a2f8ec313a52228c55d
Fernando Maclen <fmaclen@gmail.com>
2023-10-23 08:45:51 -0700

    feat: add cloudflare config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import cloudFlare from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: cloudFlare({
+			// REF https://github.com/sveltejs/kit/blob/fd6eb9b152001a537f4277a9e597aa24405a51af/documentation/docs/25-build-and-deploy/60-adapter-cloudflare.md#usage
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		}),
 	}
 };
 
