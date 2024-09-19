e851472b91e17bd3451e132464695292c98f1e1f
Fernando Maclen <fmaclen@gmail.com>
2024-09-19 19:19:00 -0400

    chore: re-organize code

--- src/lib/CurrencyInput.svelte
+++ src/lib/CurrencyInput.svelte
@@ -1,43 +1,50 @@
 <script lang="ts">
+	import {
+		DEFAULT_LOCALE,
+		DEFAULT_CURRENCY,
+		DEFAULT_NAME,
+		DEFAULT_VALUE,
+		DEFAULT_FRACTION_DIGITS,
+		DEFAULT_CLASS_WRAPPER,
+		DEFAULT_CLASS_UNFORMATTED,
+		DEFAULT_CLASS_FORMATTED,
+		DEFAULT_CLASS_FORMATTED_POSITIVE,
+		DEFAULT_CLASS_FORMATTED_NEGATIVE,
+		DEFAULT_CLASS_FORMATTED_ZERO
+	} from '$lib/constants';
+	import type { InputClasses, Callback } from '$lib/types';
 	import { onMount } from 'svelte';
 
-	const DEFAULT_LOCALE = 'en-US';
-	const DEFAULT_CURRENCY = 'USD';
-	const DEFAULT_NAME = 'total';
-	const DEFAULT_VALUE = 0;
-	const DEFAULT_FRACTION_DIGITS = 2;
-
-	const DEFAULT_CLASS_WRAPPER = 'currencyInput';
-	const DEFAULT_CLASS_UNFORMATTED = 'currencyInput__unformatted';
-	const DEFAULT_CLASS_FORMATTED = 'currencyInput__formatted';
-	const DEFAULT_CLASS_FORMATTED_POSITIVE = 'currencyInput__formatted--positive';
-	const DEFAULT_CLASS_FORMATTED_NEGATIVE = 'currencyInput__formatted--negative';
-	const DEFAULT_CLASS_FORMATTED_ZERO = 'currencyInput__formatted--zero';
-
-	interface InputClasses {
-		wrapper?: string;
-		unformatted?: string;
-		formatted?: string;
-		formattedPositive?: string;
-		formattedNegative?: string;
-		formattedZero?: string;
-	}
-
-	type Callback = (value: number) => any;
-
+	// Value and formatting
 	export let value: number = DEFAULT_VALUE;
 	export let locale: string = DEFAULT_LOCALE;
 	export let currency: string = DEFAULT_CURRENCY;
+	export let fractionDigits: number = DEFAULT_FRACTION_DIGITS;
+
+	// Input attributes
 	export let name: string = DEFAULT_NAME;
 	export let required: boolean = false;
 	export let disabled: boolean = false;
 	export let placeholder: string | number | null = DEFAULT_VALUE;
 	export let autocomplete: string | null | undefined = undefined;
+
+	// Behavior options
 	export let isNegativeAllowed: boolean = true;
 	export let isZeroNullish: boolean = false;
-	export let fractionDigits: number = DEFAULT_FRACTION_DIGITS;
+
+	// Styling
 	export let inputClasses: InputClasses | null = null;
+
+	// Callback
 	export let onValueChange: Callback = () => {};
+
+	// DOM references
+	let dom: Document;
+	let inputElement: HTMLInputElement;
+
+	// State variables
+	let formattedValue = '';
+	let inputTarget: EventTarget | null;
 
 	// Formats value as: e.g. $1,523.00 | -$1,523.00
 	const formatCurrency = (
@@ -59,11 +66,10 @@
 		const isModifier = event.metaKey || event.altKey || event.ctrlKey;
 		const isArrowKey = event.key === 'ArrowLeft' || event.key === 'ArrowRight';
 		const isTab = event.key === 'Tab';
-		const isInvalidCharacter = !/^\d|,|\.|-$/g.test(event.key); // Keys that are not a digit, comma, period or minus sign
+		const isInvalidCharacter = !/^\d|,|\.|-$/g.test(event.key);
 
-		// If there is already a decimal point, don't allow more than one
 		const isPunctuationDuplicated = () => {
-			if (event.key !== ',' && event.key !== '.') return false; // Is `false` because it's not a punctuation key
+			if (event.key !== ',' && event.key !== '.') return false;
 			if (isDecimalComma) return formattedValue.split(',').length >= 2;
 			if (!isDecimalComma) return formattedValue.split('.').length >= 2;
 			return false;
@@ -76,27 +82,25 @@
 			event.preventDefault();
 	};
 
-	// Formats the value when the input loses focus and sets the correct number of
-	// fraction digits when the value is zero
-	const handleOnBlur = () => setFormattedValue();
+	const handlePlaceholder = (placeholder: string | number | null) => {
+		if (typeof placeholder === 'number')
+			return formatCurrency(placeholder, fractionDigits, fractionDigits);
+		if (placeholder === null) return '';
+		return placeholder;
+	};
 
-	let dom: Document;
-	let inputElement: HTMLInputElement;
+	const currencyDecimal = new Intl.NumberFormat(locale).format(1.1).charAt(1);
+	const isDecimalComma = currencyDecimal === ',';
+	const currencySymbol = formatCurrency(0, 0)
+		.replace('0', '')
+		.replace(/\u00A0/, '');
 
 	onMount(() => {
 		// Set the document object as a variable so we know the page has mounted
 		dom = document;
-
 		// Set the correct fraction digits when the value is zero on initial load
 		setFormattedValue();
 	});
-
-	let inputTarget: EventTarget | null;
-	const currencyDecimal = new Intl.NumberFormat(locale).format(1.1).charAt(1); // '.' or ','
-	const isDecimalComma = currencyDecimal === ',';
-	const currencySymbol = formatCurrency(0, 0)
-		.replace('0', '') // e.g. '$0' > '$'
-		.replace(/\u00A0/, ''); // e.g '0 €' > '€'
 
 	// Updates `value` by stripping away the currency formatting
 	const setUnformattedValue = (event?: KeyboardEvent) => {
@@ -106,21 +110,20 @@
 
 			// Pressing `.` when the decimal point is `,` gets replaced with `,`
 			if (isDecimalComma && event.key === '.')
-				formattedValue = formattedValue.replace(/\.([^.]*)$/, currencyDecimal + '$1'); // Only replace the last occurence
+				formattedValue = formattedValue.replace(/\.([^.]*)$/, currencyDecimal + '$1');
 
 			// Pressing `,` when the decimal point is `.` gets replaced with `.`
 			if (!isDecimalComma && event.key === ',')
-				formattedValue = formattedValue.replace(/\,([^,]*)$/, currencyDecimal + '$1'); // Only replace the last occurence
+				formattedValue = formattedValue.replace(/\,([^,]*)$/, currencyDecimal + '$1');
 
-			// Don't format if `formattedValue` is ['$', '-$', "-"]
+			// Don't format if `formattedValue` is ['$', '-$', '-']
 			const ignoreSymbols = [currencySymbol, `-${currencySymbol}`, '-'];
 			const strippedUnformattedValue = formattedValue.replace(' ', '');
 			if (ignoreSymbols.includes(strippedUnformattedValue)) return;
 
-			// Set the starting caret positions
 			inputTarget = event.target;
 
-			// Reverse the value when minus is pressed
+			// Allow negative values if `isNegativeAllowed` is true
 			if (isNegativeAllowed && event.key === '-') value = value * -1;
 		}
 
@@ -129,22 +132,18 @@
 			? formattedValue.replace(/[^0-9,.-]/g, '')
 			: formattedValue.replace(/[^0-9,.]/g, '');
 
-		// Finally set the value
+		// If the value is not a number, set it to 0
 		if (Number.isNaN(parseFloat(unformattedValue))) {
 			value = 0;
 		} else {
-			// The order of the following operations is *critical*
-			unformattedValue = unformattedValue.replace(isDecimalComma ? /\./g : /\,/g, ''); // Remove all group symbols
-			if (isDecimalComma) unformattedValue = unformattedValue.replace(',', '.'); // If the decimal point is a comma, replace it with a period
+			// NOTE: The order of the following operations is intentional
+			unformattedValue = unformattedValue.replace(isDecimalComma ? /\./g : /\,/g, '');
+			if (isDecimalComma) unformattedValue = unformattedValue.replace(',', '.');
 
-			// If the zero-key has been pressed
-			// and if the current `value` is the same as the `value` before the key-press
-			// formatting may need to be done (Issue #30)
 			const previousValue = value;
 			value = parseFloat(unformattedValue);
 
 			if (event && previousValue === value) {
-				// Do the formatting if the number of digits after the decimal point exceeds `fractionDigits`
 				if (
 					unformattedValue.includes('.') &&
 					unformattedValue.split('.')[1].length > fractionDigits
@@ -189,14 +188,7 @@
 		onValueChange(value);
 	};
 
-	const handlePlaceholder = (placeholder: string | number | null) => {
-		if (typeof placeholder === 'number')
-			return formatCurrency(placeholder, fractionDigits, fractionDigits);
-		if (placeholder === null) return '';
-		return placeholder;
-	};
-
-	let formattedValue = '';
+	// Svelte reactive statements
 	$: isNegative = value < 0;
 	$: isPositive = value > 0;
 	$: isZero = !isNegative && !isPositive;
@@ -233,7 +225,7 @@
 		bind:value={formattedValue}
 		on:keydown={handleKeyDown}
 		on:keyup={setUnformattedValue}
-		on:blur={handleOnBlur}
+		on:blur={setFormattedValue}
 	/>
 </div>
 
---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_LOCALE = 'en-US';
+export const DEFAULT_CURRENCY = 'USD';
+export const DEFAULT_NAME = 'total';
+export const DEFAULT_VALUE = 0;
+export const DEFAULT_FRACTION_DIGITS = 2;
+
+export const DEFAULT_CLASS_WRAPPER = 'currencyInput';
+export const DEFAULT_CLASS_UNFORMATTED = 'currencyInput__unformatted';
+export const DEFAULT_CLASS_FORMATTED = 'currencyInput__formatted';
+export const DEFAULT_CLASS_FORMATTED_POSITIVE = 'currencyInput__formatted--positive';
+export const DEFAULT_CLASS_FORMATTED_NEGATIVE = 'currencyInput__formatted--negative';
+export const DEFAULT_CLASS_FORMATTED_ZERO = 'currencyInput__formatted--zero';
