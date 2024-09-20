cf2846a7930bfc71da671fc5ceccc9be44f14c5a
Fernando Maclen <fmaclen@gmail.com>
2024-09-20 11:40:03 -0400

    fix: syntax cleanup, update dependencies (#78)
    * chore: re-organize code
    
    * chore: move "Svelte-things" to the top
    
    * chore: restore the old comments
    
    * chore: update syntax for all arrow functions
    
    * chore: remove un-necessary dom variable
    
    * chore: update dependencies
    
    * chore: set node version to 20.x
    
    * chore: add optional deps for Cloudflare
    
    * chore: update playwright config
    
    * chore: update release action deps

--- src/lib/CurrencyInput.svelte
+++ src/lib/CurrencyInput.svelte
@@ -1,126 +1,134 @@
 <script lang="ts">
+	import {
+		DEFAULT_LOCALE,
+		DEFAULT_CURRENCY,
+		DEFAULT_NAME,
+		DEFAULT_VALUE,
+		DEFAULT_FRACTION_DIGITS,
+		DEFAULT_CLASS_WRAPPER,
+		DEFAULT_CLASS_UNFORMATTED,
+		DEFAULT_CLASS_FORMATTED,
+		DEFAULT_CLASS_FORMATTED_POSITIVE,
+		DEFAULT_CLASS_FORMATTED_NEGATIVE,
+		DEFAULT_CLASS_FORMATTED_ZERO
+	} from '$lib/constants';
+	import type { InputClasses, Callback } from '$lib/types';
 	import { onMount } from 'svelte';
 
-	const DEFAULT_LOCALE = 'en-US';
-	const DEFAULT_CURRENCY = 'USD';
-	const DEFAULT_NAME = 'total';
-	const DEFAULT_VALUE = 0;
-	const DEFAULT_FRACTION_DIGITS = 2;
-
-	const DEFAULT_CLASS_WRAPPER = 'currencyInput';
-	const DEFAULT_CLASS_UNFORMATTED = 'currencyInput__unformatted';
-	const DEFAULT_CLASS_FORMATTED = 'currencyInput__formatted';
-	const DEFAULT_CLASS_FORMATTED_POSITIVE = 'currencyInput__formatted--positive';
-	const DEFAULT_CLASS_FORMATTED_NEGATIVE = 'currencyInput__formatted--negative';
-	const DEFAULT_CLASS_FORMATTED_ZERO = 'currencyInput__formatted--zero';
-
-	interface InputClasses {
-		wrapper?: string;
-		unformatted?: string;
-		formatted?: string;
-		formattedPositive?: string;
-		formattedNegative?: string;
-		formattedZero?: string;
-	}
-
-	type Callback = (value: number) => any;
-
+	// Value and formatting
 	export let value: number = DEFAULT_VALUE;
 	export let locale: string = DEFAULT_LOCALE;
 	export let currency: string = DEFAULT_CURRENCY;
+	export let fractionDigits: number = DEFAULT_FRACTION_DIGITS;
+
+	// Input attributes
 	export let name: string = DEFAULT_NAME;
 	export let required: boolean = false;
 	export let disabled: boolean = false;
 	export let placeholder: string | number | null = DEFAULT_VALUE;
 	export let autocomplete: string | null | undefined = undefined;
+
+	// Behavior options
 	export let isNegativeAllowed: boolean = true;
 	export let isZeroNullish: boolean = false;
-	export let fractionDigits: number = DEFAULT_FRACTION_DIGITS;
+
+	// Styling
 	export let inputClasses: InputClasses | null = null;
+
+	// Callback
 	export let onValueChange: Callback = () => {};
 
+	// Private variables
+	let inputElement: HTMLInputElement;
+	let inputTarget: EventTarget | null;
+	let formattedValue = '';
+
+	// Svelte reactive statements
+	$: isNegative = value < 0;
+	$: isPositive = value > 0;
+	$: isZero = !isNegative && !isPositive;
+	$: value, setFormattedValue();
+
+	onMount(() => {
+		// Set the correct fraction digits when the value is zero on initial load
+		setFormattedValue();
+	});
+
 	// Formats value as: e.g. $1,523.00 | -$1,523.00
-	const formatCurrency = (
+	function formatCurrency(
 		value: number,
 		maximumFractionDigits?: number,
 		minimumFractionDigits?: number
-	) => {
+	) {
 		return new Intl.NumberFormat(locale, {
 			currency: currency,
 			style: 'currency',
 			maximumFractionDigits: maximumFractionDigits || 0,
 			minimumFractionDigits: minimumFractionDigits || 0
 		}).format(value);
-	};
+	}
 
 	// Checks if the key pressed is allowed
-	const handleKeyDown = (event: KeyboardEvent) => {
+	function handleKeyDown(event: KeyboardEvent) {
 		const isDeletion = event.key === 'Backspace' || event.key === 'Delete';
 		const isModifier = event.metaKey || event.altKey || event.ctrlKey;
 		const isArrowKey = event.key === 'ArrowLeft' || event.key === 'ArrowRight';
 		const isTab = event.key === 'Tab';
-		const isInvalidCharacter = !/^\d|,|\.|-$/g.test(event.key); // Keys that are not a digit, comma, period or minus sign
+		// Keys that are not a digit, comma, period or minus sign
+		const isInvalidCharacter = !/^\d|,|\.|-$/g.test(event.key);
 
-		// If there is already a decimal point, don't allow more than one
-		const isPunctuationDuplicated = () => {
-			if (event.key !== ',' && event.key !== '.') return false; // Is `false` because it's not a punctuation key
+		function isPunctuationDuplicated() {
+			// Is `false` because it's not a punctuation key
+			if (event.key !== ',' && event.key !== '.') return false;
 			if (isDecimalComma) return formattedValue.split(',').length >= 2;
 			if (!isDecimalComma) return formattedValue.split('.').length >= 2;
 			return false;
-		};
+		}
 
 		if (
 			isPunctuationDuplicated() ||
 			(!isDeletion && !isModifier && !isArrowKey && isInvalidCharacter && !isTab)
 		)
 			event.preventDefault();
-	};
+	}
 
-	// Formats the value when the input loses focus and sets the correct number of
-	// fraction digits when the value is zero
-	const handleOnBlur = () => setFormattedValue();
+	function handlePlaceholder(placeholder: string | number | null) {
+		if (typeof placeholder === 'number')
+			return formatCurrency(placeholder, fractionDigits, fractionDigits);
+		if (placeholder === null) return '';
+		return placeholder;
+	}
 
-	let dom: Document;
-	let inputElement: HTMLInputElement;
-
-	onMount(() => {
-		// Set the document object as a variable so we know the page has mounted
-		dom = document;
-
-		// Set the correct fraction digits when the value is zero on initial load
-		setFormattedValue();
-	});
-
-	let inputTarget: EventTarget | null;
-	const currencyDecimal = new Intl.NumberFormat(locale).format(1.1).charAt(1); // '.' or ','
+	const currencyDecimal = new Intl.NumberFormat(locale).format(1.1).charAt(1);
 	const isDecimalComma = currencyDecimal === ',';
 	const currencySymbol = formatCurrency(0, 0)
 		.replace('0', '') // e.g. '$0' > '$'
 		.replace(/\u00A0/, ''); // e.g '0 €' > '€'
 
 	// Updates `value` by stripping away the currency formatting
-	const setUnformattedValue = (event?: KeyboardEvent) => {
+	function setUnformattedValue(event?: KeyboardEvent) {
 		if (event) {
 			// Don't format if the user is typing a `currencyDecimal` point
 			if (event.key === currencyDecimal) return;
 
 			// Pressing `.` when the decimal point is `,` gets replaced with `,`
 			if (isDecimalComma && event.key === '.')
-				formattedValue = formattedValue.replace(/\.([^.]*)$/, currencyDecimal + '$1'); // Only replace the last occurence
+				// Only replace the last occurence
+				formattedValue = formattedValue.replace(/\.([^.]*)$/, currencyDecimal + '$1');
 
 			// Pressing `,` when the decimal point is `.` gets replaced with `.`
 			if (!isDecimalComma && event.key === ',')
-				formattedValue = formattedValue.replace(/\,([^,]*)$/, currencyDecimal + '$1'); // Only replace the last occurence
+				// Only replace the last occurence
+				formattedValue = formattedValue.replace(/\,([^,]*)$/, currencyDecimal + '$1');
 
-			// Don't format if `formattedValue` is ['$', '-$', "-"]
+			// Don't format if `formattedValue` is ['$', '-$', '-']
 			const ignoreSymbols = [currencySymbol, `-${currencySymbol}`, '-'];
 			const strippedUnformattedValue = formattedValue.replace(' ', '');
 			if (ignoreSymbols.includes(strippedUnformattedValue)) return;
 
-			// Set the starting caret positions
 			inputTarget = event.target;
 
-			// Reverse the value when minus is pressed
+			// Allow negative values if `isNegativeAllowed` is true
 			if (isNegativeAllowed && event.key === '-') value = value * -1;
 		}
 
@@ -129,13 +137,13 @@
 			? formattedValue.replace(/[^0-9,.-]/g, '')
 			: formattedValue.replace(/[^0-9,.]/g, '');
 
-		// Finally set the value
+		// If the value is not a number, set it to 0
 		if (Number.isNaN(parseFloat(unformattedValue))) {
 			value = 0;
 		} else {
-			// The order of the following operations is *critical*
-			unformattedValue = unformattedValue.replace(isDecimalComma ? /\./g : /\,/g, ''); // Remove all group symbols
-			if (isDecimalComma) unformattedValue = unformattedValue.replace(',', '.'); // If the decimal point is a comma, replace it with a period
+			// NOTE: The order of the following operations is intentional
+			unformattedValue = unformattedValue.replace(isDecimalComma ? /\./g : /\,/g, '');
+			if (isDecimalComma) unformattedValue = unformattedValue.replace(',', '.');
 
 			// If the zero-key has been pressed
 			// and if the current `value` is the same as the `value` before the key-press
@@ -153,14 +161,14 @@
 				}
 			}
 		}
-	};
+	}
 
-	const setFormattedValue = () => {
+	function setFormattedValue() {
 		// Do nothing because the page hasn't mounted yet
-		if (!dom) return;
+		if (!inputElement) return;
 
 		// Previous caret position
-		const startCaretPosition = inputElement?.selectionStart || 0;
+		const startCaretPosition = inputElement.selectionStart || 0;
 		const previousFormattedValueLength = formattedValue.length;
 
 		// Apply formatting to input
@@ -170,7 +178,7 @@
 				: formatCurrency(
 						value,
 						fractionDigits,
-						dom.activeElement === inputElement ? 0 : fractionDigits
+						document.activeElement === inputElement ? 0 : fractionDigits
 				  );
 
 		// Update `value` after formatting
@@ -187,20 +195,7 @@
 
 		// Run callback function when `value` changes
 		onValueChange(value);
-	};
-
-	const handlePlaceholder = (placeholder: string | number | null) => {
-		if (typeof placeholder === 'number')
-			return formatCurrency(placeholder, fractionDigits, fractionDigits);
-		if (placeholder === null) return '';
-		return placeholder;
-	};
-
-	let formattedValue = '';
-	$: isNegative = value < 0;
-	$: isPositive = value > 0;
-	$: isZero = !isNegative && !isPositive;
-	$: value, setFormattedValue();
+	}
 </script>
 
 <div class={inputClasses?.wrapper ?? DEFAULT_CLASS_WRAPPER}>
@@ -233,7 +228,7 @@
 		bind:value={formattedValue}
 		on:keydown={handleKeyDown}
 		on:keyup={setUnformattedValue}
-		on:blur={handleOnBlur}
+		on:blur={setFormattedValue}
 	/>
 </div>
 
---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+export interface InputClasses {
+  wrapper?: string;
+  unformatted?: string;
+  formatted?: string;
+  formattedPositive?: string;
+  formattedNegative?: string;
+  formattedZero?: string;
+}
+
+export type Callback = (value: number) => any;
